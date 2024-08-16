7a2d7355d5e9302ab9480e6dde9db70b92f159f9
ibiacmbyww <natalie.driscoll5@gmail.com>
2024-08-16 01:24:56 +0100

    Zoom in and out work, but can't go to the extremes of the scale

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { WheelEvent, useEffect, useRef, useState } from 'react';
+import React, { WheelEvent, useCallback, useEffect, useRef, useState } from 'react';
 import bg1 from "./img/bg1.png";
 import './App.scss';
 import { MouseEventHandler } from 'react';
@@ -12,140 +12,99 @@ function App() {
   const [minZoomLevel, setMinZoomLevel] = useState<number>(1)
   const [maxZoomLevel, setMaxZoomLevel] = useState<number>(1)
   const [zoomLevelSteps] = useState<number>(30)
-  const [bg1Initialised, setBG1Initialised] = useState<boolean>(false)
+  // const [bg1Initialised, setBG1Initialised] = useState<boolean>(false)
   const [zoomIncrementBy, setZoomIncrementBy] = useState<number>(0)
   const [scrollX, setScrollX] = useState<number>(0)
   const [scrollY, setScrollY] = useState<number>(0)
   const [bg1WidthAtDefaultZoom, setBG1WidthAtDefaultZoom] = useState<number>(0)
-  const [leftMouseDown, setLeftMouseDown] = useState<boolean>(false)
-  const [middleMouseDown, setMiddleMouseDown] = useState<boolean>(false)
-  const [rightMouseDown, setRightMouseDown] = useState<boolean>(false)
+  // const [leftMouseDown, setLeftMouseDown] = useState<boolean>(false)
+  // const [middleMouseDown, setMiddleMouseDown] = useState<boolean>(false)
+  // const [rightMouseDown, setRightMouseDown] = useState<boolean>(false)
   const [actors, setActors] = useState<Actor[]>([
     {
       id: 0,
-      displayName: "Maaaarv",
+      displayName: "Alicia",
       playerName: "Alice",
       posX: 1800,
       posY: 600
     }
   ])
-  // const [viewOffsetX, setViewOffsetX] = useState(0);
-  // const [viewOffsetY, setViewOffsetY] = useState(0);
-  const bg1Ref = useRef<HTMLImageElement>(null)
-  
-  const bgScrollHandler = (ev: globalThis.WheelEvent) => {
-    ev.preventDefault()
-    debugger;
-    console.log(ev)
-    if (ev.deltaY < 0) {
-      //is scroll UP
-      if ((zoomLevel + zoomIncrementBy) <= maxZoomLevel) {
-        setZoomLevel((z) => z + zoomIncrementBy)
-      }
-    } else {
-      //is scroll DOWN
-      if ((zoomLevel - zoomIncrementBy) >= minZoomLevel) {
-        setZoomLevel((z) => z - zoomIncrementBy)
-      }
-    }
-    // setViewOffsetX(window.scrollX)
-    // setViewOffsetY(window.scrollY)
-  }
-  const handleWheel = function(this: HTMLImageElement, ev: Event): void {
-    ev.preventDefault();
-    const event = ev;
-    debugger;
-    if ("deltaY" in ev) {
-      //@ts-ignore-next-line
-      if (event.deltaY < 0) {
-        //is scroll UP
-        if ((zoomLevel + zoomIncrementBy) <= maxZoomLevel) {
-          setZoomLevel((z) => z + zoomIncrementBy)
-        }
-      } else {
-        //is scroll DOWN
-        if ((zoomLevel - zoomIncrementBy) >= minZoomLevel) {
-          setZoomLevel((z) => z - zoomIncrementBy)
-        }
-      }
-    }
-  };
 
-  const useNonPassiveWheel = (handler: (this: HTMLImageElement, event: Event) => void) => {
-    const elementRef = useRef<HTMLImageElement>(null);
-  
-    useEffect(
-      () => {
-        const element = elementRef.current;
-        if (element) {
-          debugger;
-          const windowWidth = window.innerWidth
-          const imgWidth = element.clientWidth ?? 0
-          setZoomLevel(1)
-          const gridColumnsShown = windowWidth / fiveFtInPx;
-          const zoomLevelWhenThreeGridColumnsShown = parseFloat((1/(3/gridColumnsShown)).toPrecision(3))
-          setMaxZoomLevel(zoomLevelWhenThreeGridColumnsShown)
-          const imgWidthInColumns = imgWidth / fiveFtInPx
-          const zoomLevelWhenAllGridColumnsShown = parseFloat((1/(imgWidthInColumns/gridColumnsShown)).toPrecision(3))
-          setMinZoomLevel(zoomLevelWhenAllGridColumnsShown)
-          const zoomTick = (zoomLevelWhenThreeGridColumnsShown - zoomLevelWhenAllGridColumnsShown) / zoomLevelSteps
-          setZoomIncrementBy(zoomTick)
-          setBG1Initialised(true)
-          element.removeEventListener('wheel', handler);
-          element.addEventListener('wheel', handler, { passive: false });
-        }
-        return () => {
-          if (element) {
-            element.removeEventListener('wheel', handler);
+  const bgRef = useRef<HTMLImageElement>(null);
+
+  function bgLoadedHandler() {
+    const element = bgRef.current;
+    if (element) {
+      const windowWidth = window.innerWidth
+      const imgWidth = element.naturalWidth ?? 0
+      setBG1WidthAtDefaultZoom(imgWidth)
+      setZoomLevel(1)
+      const gridColumnsShown = windowWidth / fiveFtInPx;
+      const zoomLevelWhenThreeGridColumnsShown = parseFloat((1/(3/gridColumnsShown)).toPrecision(3))
+      const imgWidthInColumns = imgWidth / fiveFtInPx
+      const zoomLevelWhenAllGridColumnsShown = parseFloat((1/(imgWidthInColumns/gridColumnsShown)).toPrecision(3))
+      const zoomTick = (zoomLevelWhenThreeGridColumnsShown - zoomLevelWhenAllGridColumnsShown) / zoomLevelSteps
+      setMaxZoomLevel(zoomLevelWhenThreeGridColumnsShown)
+      setMinZoomLevel(zoomLevelWhenAllGridColumnsShown)
+      setZoomIncrementBy(zoomTick)
+      // element.removeEventListener('wheel');
+    }
+  }
+
+  const hw = useCallback(
+    (ev: Event): void => {
+      ev.preventDefault();
+      const event = ev;
+      debugger;
+      if ("deltaY" in ev) {
+        //@ts-ignore-next-line
+        if (event.deltaY < 0) {
+          //is scroll UP
+          if ((zoomLevel + zoomIncrementBy) <= maxZoomLevel) {
+            setZoomLevel((z) => z + zoomIncrementBy)
           }
-        };
-      },
-      [handler]
-    )
-  
-    return elementRef;
-  }
+        } else {
+          //is scroll DOWN
+          if ((zoomLevel - zoomIncrementBy) > minZoomLevel) {
+            setZoomLevel((z) => z - zoomIncrementBy)
+          }
+        }
+      }
+    },
+    [maxZoomLevel, minZoomLevel, zoomIncrementBy, zoomLevel]
+  )
 
-  const bgRef = useNonPassiveWheel(handleWheel);
-
-  const bgMouseDown: MouseEventHandler = (e) => {
-    if (e.button === 0) {
-      //left click
-    // debugger;
+  useEffect(() => {
+    debugger;
+    const element = bgRef.current;
+    if (element) {
+      debugger;
+      element.addEventListener(
+        'wheel',
+        hw,
+        { passive: false }
+      )
     }
-    if (e.button === 1) {
-      //middle click
-    // debugger;
-    }
-    if (e.button === 2) {
-      //right click
-    // debugger;
-    }
-  }
-
-  const bgMouseUp: MouseEventHandler = (e) => {
-    if (e.button === 0) {
-      //left click
-    // debugger;
-    }
-    if (e.button === 1) {
-      //middle click
-    // debugger;
-    }
-    if (e.button === 2) {
-      //right click
-    // debugger;
-    }
-  }
-
-  const bgMouseMove: MouseEventHandler = (e) => {
-    // debugger;
-  }
+    return () => {
+      if (element) {
+        element.removeEventListener('wheel', hw);
+      }
+    };
+  }, [zoomLevel, hw])
 
   debugger;
   return (
     <div className="App">
-      <img src={bg1} onMouseDown={bgMouseDown} onMouseUp={bgMouseUp} onMouseMove={bgMouseMove} ref={bgRef} style={{minWidth: `${(bg1WidthAtDefaultZoom ?? window.innerWidth) * zoomLevel}px`}} />
+      <img
+        src={bg1}
+        ref={bgRef}
+        style={
+          {
+            minWidth: `${(bg1WidthAtDefaultZoom ?? window.innerWidth) * zoomLevel}px`,
+            maxWidth: `${(bg1WidthAtDefaultZoom ?? window.innerWidth) * zoomLevel}px`
+          }
+        }
+        onLoad={bgLoadedHandler} />
       {actors.map(
         (actor) => {
           return <div className="actor"
